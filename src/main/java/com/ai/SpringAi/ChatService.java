6824830239f3afda32b1c4c24d1d1c2f61369a16
Sujal Shrestha <shresthasujal2021@gmail.com>
2026-06-01 21:14:17 +0545

    Error Occur

---
+++ src/main/java/com/ai/SpringAi/ChatService.java
@@ -0,0 +1,12 @@
+package com.ai.SpringAi;
+
+
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.openai.api.OpenAiApi;
+
+public class ChatService {
+    private final OpenAiApi.ChatModel chatModel;
+    public ChatService(ChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+}
