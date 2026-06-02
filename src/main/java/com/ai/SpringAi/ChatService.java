0444a365cfbd7aed2d109c6ad7a65a80df06eb10
Sujal Shrestha <shresthasujal2021@gmail.com>
2026-06-02 22:26:45 +0545

    ChatService initialized

--- src/main/java/com/ai/SpringAi/ChatService.java
+++ src/main/java/com/ai/SpringAi/ChatService.java
@@ -5,8 +5,15 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.openai.api.OpenAiApi;
 
 public class ChatService {
-    private final OpenAiApi.ChatModel chatModel;
+
+    private final ChatModel chatModel;
+
     public ChatService(ChatModel chatModel) {
         this.chatModel = chatModel;
+    }
+
+    public String getResponse(String prompt) {
+        return chatModel.call(prompt);
+
     }
 }
