f72f9f0ce901e2c17b88ed4b0620e3d9cf12311d
Sujal Shrestha <shresthasujal2021@gmail.com>
2026-06-08 21:25:57 +0545

    ImageService

---
+++ src/main/java/com/ai/SpringAi/service/ImageService.java
@@ -0,0 +1,22 @@
+package com.ai.SpringAi.service;
+
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.image.ImageResponse;
+import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageService {
+    private final OpenAiImageModel  openAiImageModel;
+
+    public ImageService(OpenAiImageModel openAiImageModel) {
+        this.openAiImageModel = openAiImageModel;
+    }
+
+    public ImageResponse generateImage(String prompt) {
+        ImageResponse imageResponse = openAiImageModel.call(
+                new ImagePrompt(prompt)
+        );
+        return imageResponse;
+    }
+}
