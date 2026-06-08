b8acb17be63a52d1554b654ddf278d88f6758e13
Sujal Shrestha <shresthasujal2021@gmail.com>
2026-06-08 21:31:53 +0545

    Added Generate Image Getpoint

--- src/main/java/com/ai/SpringAi/controller/GenAiController.java
+++ src/main/java/com/ai/SpringAi/controller/GenAiController.java
@@ -1,17 +1,24 @@
 package com.ai.SpringAi.controller;
 
 import com.ai.SpringAi.service.ChatService;
+import com.ai.SpringAi.service.ImageService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.ai.image.ImageResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class GenAiController {
 
     private final ChatService chatService;
+    private final ImageService imageService;
 
-    public GenAiController(ChatService chatService) {
+    public GenAiController(ChatService chatService, ImageService imageService) {
         this.chatService = chatService;
+        this.imageService = imageService;
     }
 
     @GetMapping("/ask-ai")
@@ -22,5 +29,12 @@ public class GenAiController {
     @GetMapping("/ask-ai-options")
     public String getResponseOptions(@RequestParam String prompt) {
         return chatService.getResponseOptions(prompt);
+    }
+
+    @GetMapping("/generate-image")
+    public void generateImages(HttpServletResponse response, @RequestParam String prompt) throws IOException {
+        ImageResponse imageResponse = imageService.generateImage(prompt);
+        String imageUrl = imageResponse.getResult().getOutput().getUrl();
+        response.sendRedirect(imageUrl);
     }
 }
